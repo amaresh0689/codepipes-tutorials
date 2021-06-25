279e4ce998ed8beab3bd58b499b9e854fddebb25
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-06-25 10:24:53 +0530

    Update aws_s3.tf

--- html-demo/aws/aws_s3.tf
+++ html-demo/aws/aws_s3.tf
@@ -1,45 +1,12 @@
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = "hello-vang-${random_string.random.result}-${replace(lower(var.what_to_say)," ","-")}"
-  acl    = "public-read"
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
 
-  force_destroy = true
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+# ubuntu-trusty-14.04 (x64)
+variable "aws_amis" {
+  default = {
+    "us-east-1" = "ami-5f709f34"
+    "us-west-2" = "ami-7f675e4f"
   }
-}
-
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "Public-Access",
-  "Statement": [
-    {
-      "Sid": "Allow-Public-Access-To-Bucket",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": [
-          "arn:aws:s3:::${aws_s3_bucket.website_bucket.bucket}/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
-
-resource "local_file" "index_html_aws" {
-    content     = templatefile("${path.module}/../skins/${var.skin}.html.tmpl", {orgname = var.orgname, what_to_say = replace(var.what_to_say, " ", "%20")})
-    filename = "${path.module}/index.html"
-}
-
-resource "aws_s3_bucket_object" "object" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = "${path.module}/index.html"
-  content_type = "text/html"
 }
